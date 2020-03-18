7c00c06b2d1811258aff9f114af093597871d4f4
DiaZa13 <cor191025@uvg.edu.gt>
2020-03-17 20:45:34 -0600

    added comments

--- Node.java
+++ Node.java
@@ -1,6 +1,15 @@
-
+/**
+ * 
+ * @author Diana Zaray Corado Lopez #191025
+ * @version 17/03/2020
+ * @author Marcos Lopes Gonzalez
+ * La siguiente clase fue basada en @see https://www.baeldung.com/java-binary-tree
+ * @param <K> key of the association
+ * @param <V> value of the association
+ */
 public class Node<K extends Comparable<K>,V> {
 	
+	//Auxiliar class that store the associations and keep a reference to each child
 	protected ComparableAssociation<K,V> word;
 	protected Node left;
 	protected Node right;
